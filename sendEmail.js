45d9ed7eea16a0d792506c240081476dca54baa7
jhoncena86 <asadburki68@gmail.com>
2025-07-04 01:23:04 +0400

    Delete sendEmail.js

--- sendEmail.js
+++
@@ -1,9 +0,0 @@
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email, message } = req.body;
-    console.log("Email from:", email, "Name:", name, "Message:", message);
-    return res.status(200).json({ success: true });
-  }
-  res.status(405).end();
-}
